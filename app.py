d3d4c82f01c2834ccb2c64aaf1690be2c97e7a97
MARWAN ELSABIE <marwan.2000@windowslive.com>
2025-04-27 01:04:42 +0300

    created render.yaml and build.sh

--- app.py
+++ app.py
@@ -383,5 +383,5 @@ with app.app_context():
         db.session.add(admin)
         db.session.commit()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
